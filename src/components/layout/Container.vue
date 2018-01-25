703e1e90d2b68326da6eb9d0eb03300546a83003
aa1479474496 <aa1479474496@qq.com>
2018-01-25 15:21:07 +0800

    vue-updated

--- src/components/layout/Container.vue
+++ src/components/layout/Container.vue
@@ -2,6 +2,10 @@
   <div class='main' :class="siderClass">
     <nav-header></nav-header>
     <bread-crumb />
+    <button @click="addCounter">点击加数</button>
+    <ul>
+      <li v-for="item in num">{{item}}</li>
+    </ul>
     <div>
       <router-view/>
     </div>
@@ -17,12 +21,31 @@
       NavHeader,
       BreadCrumb
     },
+    data() {
+      return {
+        msg: 'hello',
+        num: [1]
+      }
+    },
     computed: {
       ...mapGetters([
         'siderClass'
       ])
     },
+    created() {
+      console.log('created');
+      console.log(this.msg);
+    },
     mounted() {
+      console.log('mounted');
+    },
+    updated() {
+      console.log('数据更新了');
+    },
+    methods: {
+      addCounter() {
+        this.num.push(Math.round(Math.random()*10 + 10))
+      }
     }
   }
 </script>
